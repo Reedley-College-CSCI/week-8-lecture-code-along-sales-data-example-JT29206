b186202f4d678701e84269f0c058e21422f52404
jaslene.thomas4@gmail.com <jaslene.thomas4@gmail.com>
2026-03-10 10:01:56 -0700

    Added for loop to display elements in array

--- sales_data.cpp
+++ sales_data.cpp
@@ -2,6 +2,8 @@
 //Week 8 Demo
 #include <iostream>
 using namespace std;
+
+void showDay();
 int main() {
     const int MAX_SIZE = 10;
     int numDays = 7;
@@ -9,6 +11,24 @@ int main() {
     double amounts[MAX_SIZE] = { 1200, 1800, 900, 1500, 800, 1100, 1300 };
 
     cout << "Welcome to Store Sales Data Analysis!" << endl;
-    cout << "Day 1 sales: " << amounts[0] << endl;
+   // cout << "Day 1 sales: " << amounts[0] << endl;
+   // cout << "Day 2 sales: " << amounts[1] << endl;
+    /*
+    showDay();
+    showDay();
+    */
+
+    //cout << "Array memory: ~" << sizeof(amounts) << " bytes" << endl;
+
+    for (int i = 0; i < numDays; i++) {
+        cout << "Day " << i + 1 << ": $" << amounts[i] << endl;
+    }
     return 0;
+}
+
+
+void showDay() {
+    static int dayNum = 1; // Persists
+    cout << "Showing day " << dayNum << endl;
+    dayNum++;
 }
